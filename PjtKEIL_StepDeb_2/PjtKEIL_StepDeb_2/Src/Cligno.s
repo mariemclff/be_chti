929c211ae73e01e33c7a735d0627e8838e9b9778
Mecaliff Marie <mecaliff@insa-20671>
2022-03-25 12:56:04 +0100

    first commit

---
+++ PjtKEIL_StepDeb_2/PjtKEIL_StepDeb_2/Src/Cligno.s
@@ -0,0 +1,77 @@
+	PRESERVE8
+	THUMB   
+		
+    import GPIOB_Set
+	import GPIOB_Clear
+	export timer_callback
+; ====================== zone de réservation de données,  ======================================
+;Section RAM (read only) :
+	area    mesdata,data,readonly
+ 
+
+;Section RAM (read write):
+	area    maram,data,readwrite
+		
+
+;char FlagCligno;
+		
+FlagCligno dcd 0
+
+	
+; ===============================================================================================
+	
+
+
+		
+;Section ROM code (read only) :		
+	area    moncode,code,readonly
+; écrire le code ici		
+
+	
+;void timer_callback(void)
+;{
+
+timer_callback proc
+;	if (FlagCligno==1)
+;	{
+
+	ldr r1, =FlagCligno
+	ldr r0, [r1]
+	cmp r0, #1
+	beq toto
+	
+	
+;	}
+;	else
+;	{
+	
+;		FlagCligno=1;
+	mov r0, #1
+	str r0,[r1]
+;		GPIOB_Clear(1);
+	mov r0, #1
+	bl GPIOB_Clear
+	bx lr
+;	}
+
+toto
+;		FlagCligno=0;
+	mov r0, #0
+	str r0,[r1]
+;		GPIOB_Set(1);
+	mov r0, #1
+	bl GPIOB_Set
+	bx lr
+	
+	endp
+;
+;
+;
+;
+
+
+
+
+		
+		
+	END	
